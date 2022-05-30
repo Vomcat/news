f9f28a8997ee5363d4a2ba6acc59adc3734f6b4c
Weronika Koczwara <weronika.koczwara@vmlyr.com>
2022-05-30 20:15:56 +0200

    Add article hero

--- components/ArticleHero/ArticleHero.js
+++ components/ArticleHero/ArticleHero.js
@@ -1,13 +1,14 @@
 import styles from './ArticleHero.module.scss'
-import Link from 'next/link'
 
-
-const ArticleHero = ({ title, articleBody, url }) => {
+const ArticleHero = ({ title, articleBody, img }) => {
     return (
         <div className={styles['article']}>
             <div className='container sm'>
                 <h1 className={styles['article__title']}>{title}</h1>
-                {articleBody.map(bodyData =>(<div className={styles["article__content"]} dangerouslySetInnerHTML={{ __html: bodyData.data}}></div>))}
+                <picture>
+                    <img className={styles['article__image']} src={img}/>
+                </picture>
+                {articleBody.map((bodyData, index) =>(<div key={index} className={styles["article__content"]} dangerouslySetInnerHTML={{ __html: bodyData.data}}></div>))}
             </div>
         </div>
     )
